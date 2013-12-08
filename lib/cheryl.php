6ba04d754844c411c42af4f1a75f01ea51384339
arzynik <github@arzynik.com>
2013-12-07 18:39:54 -0800

    fixed default timezone issue

--- lib/cheryl.php
+++ lib/cheryl.php
@@ -6,6 +6,8 @@ error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ignore_user_abort(false);
 ini_set('display_errors',true);
 set_time_limit(10);
+date_default_timezone_set('America/Los_Angeles');
+
 ini_set('zlib.output_compression','On');
 ini_set('zlib.output_compression_level', 9);
 
