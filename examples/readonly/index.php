e3ed1ae2690baf69834ceb54fe4c819ebe37af19
arzynik <github@arzynik.com>
2013-12-10 15:59:14 -0800

    added readonly example

---
+++ examples/readonly/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Basic Cheryl example of using a separate index script
+ *
+ * uses static cheryl methods
+ *
+ */
+
+
+// if CHERYL_CONFIG is defined, the script will not automatilcy run
+define('CHERYL_CONTROL', true);
+
+// if you want better password hashing, put something here
+define('CHERYL_SALT', 'SOMETHING/NOT/COOL/AND/RANDOM');
+
+// include the Cheryl libraries
+require_once('../../build/Cheryl.php');
+
+// give Cheryl our config. this will merge with the default config
+Cheryl::init(array(
+	'root' => '../files',
+	'readonly' => true
+));
+
+// manualy run the script since were using a custom config
+Cheryl::go();
